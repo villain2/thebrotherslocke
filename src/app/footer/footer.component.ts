5528d6227a9c4476a3be38847a77a525745b70a3
K.C. Hunter <kchunter@connectionseducation.com>
2016-09-29 11:45:15 -0400

    adding collapse tray button

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,11 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, trigger, state, style, transition, animate } from '@angular/core';
 import { Episode } from '../episodes/episode';
 import { EpisodesService } from '../episodes.service';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  providers: [ EpisodesService ]
+  providers: [ EpisodesService ],
+  animations: [
+    trigger('collapseOut', [
+      state('inactive', style({
+        marginTop: 300
+      })),
+      state('active', style({
+        marginTop: 0
+      })),
+      transition('inactive => active', animate('100ms ease-in')),
+      transition('active => inactive', animate('100ms ease-out'))
+    ])
+  ]
 })
 export class FooterComponent implements OnInit {
 
@@ -27,6 +39,11 @@ export class FooterComponent implements OnInit {
         episodes => this.episodes = episodes,
         error => this.errorMessage = <any>error
       );
+  }
+
+  collapseTray ()
+  {
+    console.log('collapse tray');
   }
 
 }
