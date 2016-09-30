93adf308c568c29271479b458b2ed748b4f4ba58
K.C. Hunter <kchunter@connectionseducation.com>
2016-09-30 14:50:57 -0400

    accessing the sidebar DOM object through the nativeElement options in Angular2.

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,25 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ElementRef, ViewChild, Renderer} from '@angular/core';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html'
 })
-export class SidebarComponent implements OnInit {
+export class SidebarComponent implements OnInit
+{
+  @ViewChild('controlSidebar') input: ElementRef;
 
-  constructor() { }
+  constructor(private renderer: Renderer) { }
 
   private expanded: boolean;
-
-  expanded  = false;
+  public sidebarObj: any;
 
   ngOnInit() {
-
+    this.expanded  = false;
   }
+
 
   private over ()
   {
+    console.log(this);
+    this.sidebarObj    = this.input.nativeElement;
+    console.log(this.sidebarObj);
+
     //check if the sidebar is expanded or not
-    if(!expanded)
+    if(!this.expanded)
     {
 
     }
