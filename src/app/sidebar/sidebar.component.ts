3339dbb40e9cbf0f6069d66de94f2a500d3f728d
K.C. Hunter <kchunter@connectionseducation.com>
2016-10-03 18:01:56 -0400

    adding add and close button defaults.

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -11,6 +11,11 @@ import {Component, OnInit, ElementRef, ViewChild, Renderer, trigger, state, styl
       state('open', style({
         right: '0px'
       })),
+      state('indicate', style({
+        right: '-173px'
+      })),
+      transition('collapsed => indicate', animate('0.5s 0.5s cubic-bezier(0.77, 0, 0.175, 1)')),
+      transition('indicate => collapsed', animate('0.5s 0.5s cubic-bezier(0.77, 0, 0.175, 1)')),
       transition('collapsed => open', animate('0.5s 0.5s cubic-bezier(0.77, 0, 0.175, 1)')),
       transition('open => collapsed', animate('0.5s 2s cubic-bezier(0.77, 0, 0.175, 1)'))
     ])
@@ -41,7 +46,7 @@ export class SidebarComponent implements OnInit
     {
       //this.sidebarObj.setAttribute("style", "right: 0;");
       this.expanded   = true;
-      this.stateExpression  = 'open';
+      this.stateExpression  = 'indicate';
     }
   }
 
@@ -52,5 +57,21 @@ export class SidebarComponent implements OnInit
     //this.sidebarObj.setAttribute("style", "right: -183px");
     this.stateExpression  = 'collapsed';
   }
+
+  public sideTrayOpen ()
+  {
+    console.log('open sidebar');
+    this.expanded   = true;
+    this.stateExpression = 'open';
+  }
+
+  public sideTrayClose ()
+  {
+    console.log('close sidebar');
+    this.expanded   = false;
+    this.stateExpression   = 'collapsed';
+
+  }
+
 
 }
