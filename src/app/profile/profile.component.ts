9c2a47de3a5ec70abec1cdd89697a9f0dede06b7
K.C. Hunter <kchunter@connectionseducation.com>
2016-10-17 01:53:39 -0400

    updating profile for fix for Facebook login

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -11,7 +11,8 @@ export class ProfileComponent implements OnInit {
 
   token: any;
   logged: boolean   = false;
-  user  = { };
+  user  = { name: 'Your Name'};
+  progress: number  = 0;
 
   constructor(private _ngZone: NgZone) { }
 
@@ -20,13 +21,48 @@ export class ProfileComponent implements OnInit {
     if(response.status === 'connected')
     {
       console.log('connected');
-      this.me();
+
+      this._ngZone.runOutsideAngular(() => {
+        this._increaseProgress(() => {
+          // reenter the Angular zone and display done
+          console.log('increase progress');
+          this._ngZone.run(() => {
+            console.log('Outside Done!')
+          });
+        })
+      });
+
+
+      this._ngZone.run(() => {
+        this.me();
+      });
     }
     else
     {
       this.login();
     }
   }
+
+  _increaseProgress(callback)
+  {
+    this.progress += 1;
+
+
+    FB.api('/me?fields=id,name,first_name,gender,picture.width(150).height(150),age_range,friends',
+      function(result) {
+        if (result && !result.error) {
+          this.user = result;
+          console.log(this.user);
+        } else {
+          console.log(result.error);
+        }
+      });
+
+    console.log(this.user);
+    console.log('progress: ' + this.progress);
+
+  }
+
 
   login ()
   {
