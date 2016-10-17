b56545a8e12b3045d861a44e54a582abbb2d366e
K.C. Hunter <kchunter@connectionseducation.com>
2016-10-17 01:13:35 -0400

    Updating for Facebook login.

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, NgZone } from '@angular/core';
+
+declare const FB: any;
 
 @Component({
   selector: 'app-profile',
@@ -7,9 +9,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+  token: any;
+  logged: boolean   = false;
+  user  = { };
+
+  constructor(private _ngZone: NgZone) { }
+
+  statusChangeCallback ( response: any)
+  {
+    if(response.status === 'connected')
+    {
+      console.log('connected');
+      this.me();
+    }
+    else
+    {
+      this.login();
+    }
+  }
+
+  login ()
+  {
+    FB.login((result: any) => {
+      this.logged = true;
+      this.token = result;
+    }, { scope: 'user_friends' });
+  }
+
+  me() {
+    FB.api('/me?fields=id,name,first_name,gender,picture.width(150).height(150),age_range,friends',
+      function(result) {
+        if (result && !result.error) {
+          this.user = result;
+          console.log(this.user);
+          console.log(this.logged);
+        } else {
+          console.log(result.error);
+        }
+      });
+  }
+
 
   ngOnInit() {
+    FB.getLoginStatus(response => {
+      this.statusChangeCallback(response);
+    });
   }
 
 }
