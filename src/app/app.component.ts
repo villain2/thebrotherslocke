a5189394c9de156eea3a1931dbcafea5a316b82e
K.C. Hunter <kchunter@connectionseducation.com>
2016-10-30 20:19:50 -0400

    adding about page updates.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import {AfterViewInit, ViewChild } from '@angular/core';
 import {Component, OnInit} from '@angular/core';
 import { FooterComponent } from './footer/footer.component';
+import { WindowRef } from './WindowRef';
 import './rxjs-operators';
 
 
