3e1343da085a98eedc238bffe275ac4ffe719ca2
djvillain <nwo4life>
2016-11-16 22:56:34 -0500

    initial game setup continued.

--- src/app/safe-game-old/lock-one/lock-one.component.ts
+++ src/app/safe-game-old/lock-one/lock-one.component.ts
@@ -11,20 +11,40 @@ export class LockOneComponent implements AfterViewInit {
 
   /** set variables **/
   private canvas: any;
+  private context: any;
+  private isMobile: any;
+  private stage: any;
+  private DEFAULT_WIDTH: number   = 900;
+  private DEFAULT_HEIGHT: number  = 550;
+  private FRAMERATE: number       = 60;
+  private rowOnePanels: any;
+  private rowTwoPanels: any;
+  private rowThreePanels: any;
+  private player: any;
 
   @ViewChild("world") world: ElementRef;
 
   constructor() {}
 
-
   ngAfterViewInit() {
-    this.canvas   = this.world.nativeElement;
-    console.log(this.canvas);
-    this.canvas.width   = 900;
-    this.canvas.height  = 550;
+    //flag if we're on mobile
+    this.isMobile = !!navigator.userAgent.toLowerCase().match(/ipod|ipad|iphone|android/gi);
 
+    /** set up the world **/
+    this.stage = {
+      width: this.isMobile ? window.innerWidth : this.DEFAULT_WIDTH,
+      height: this.isMobile ? window.innerHeight : this.DEFAULT_HEIGHT
+    };
+
+    this.canvas   = this.world.nativeElement;
+
+    /** init game **/
     if(this.canvas && this.world.nativeElement.getContext)
     {
+      this.context  = this.canvas.getContext('2d');
+
+      //register event listeners
+      document.addEventListener('mousemove', this.docMouseMoveHandler, false);
       this.canvas.fillStyle   = '#ff0000';
     }
     /*this.canvas.fillStyle = '#ff0000';
@@ -36,6 +56,11 @@ export class LockOneComponent implements AfterViewInit {
 
 
     this.tick();
+  }
+
+  docMouseMoveHandler ()
+  {
+
   }
 
   tick()
